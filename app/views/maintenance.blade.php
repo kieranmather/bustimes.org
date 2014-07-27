982e3efe94d87b80dee5995bf120fa78fe6fc49e
Kieran Mather <kieran.mather@googlemail.com>
2014-07-27 16:30:10 +0100

    Added pretty maintenance page for when importing data

---
+++ app/views/maintenance.blade.php
@@ -0,0 +1,19 @@
+@extends('master')
+
+
+@section('header')
+		<h1 class="text-center">Bustimes.org</h1>
+		<h3 class="text-center">Bus times for any stop in Great Britain</h3>
+@stop
+
+@section('content')
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="alert alert-warning">
+					<h3>Maintenance</h3>
+					<p>The site's down for maintenance at the moment. We'll be really quick but you can call Traveline on 0871 200 22 33* if you need information now.</p>
+					<p><small>*Calls cost 10p per minute plus network extras.</small></p>
+				</div>
+			</div>
+		</div>
+@stop
