eaba70b8521675a59b8829ea30c4d8ebd1017575
Kieran Mather <kieran.mather@googlemail.com>
2014-09-21 17:36:50 +0100

    Defering JS load because webscale

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -1,12 +1,6 @@
 @extends('master')
 
 
-@section('htmlhead')
-	{{HTML::script('assets/bustimes.org/geo.js')}}
-	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPoTi3VIEkmiFhyMoprykJOIIn4w6lBgE&amp;sensor=false"></script>
-@stop
-
-
 @section('header')
 		<h1 class="text-center">Bustimes.org</h1>
 		<h3 class="text-center">Bus times for any stop in Great Britain</h3>
@@ -40,6 +34,8 @@
 
 @section('footer')
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+{{HTML::script('assets/bustimes.org/geo.js')}}
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPoTi3VIEkmiFhyMoprykJOIIn4w6lBgE&amp;sensor=false"></script>
 <script type="text/javascript">
 	$("#searchForm").submit(function(event){
 
